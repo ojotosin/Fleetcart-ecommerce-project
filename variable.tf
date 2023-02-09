97e72ca73347083cf50f1aecd911319bd10454a1
Tosin Ojo <ojotosin1900@gmail.com>
2023-02-09 17:13:48 -0500

    adding route53.tf file and modifying the variable.tf file

--- variable.tf
+++ variable.tf
@@ -57,82 +57,96 @@ variable "private_datasubnetAZ2_cidr" {
 
 # security group variables
 variable "ssh_location" {
-    default = "0.0.0.0/0"
-    description = "ip from which ssh is allowed"
-    type = string
+    default             = "0.0.0.0/0"
+    description         = "ip from which ssh is allowed"
+    type                = string
   
 }
 
 # rds variables
 variable "database_snapshot_identifier" {
-    default = "arn:aws:rds:us-east-1:774443160673:snapshot:fleetcart-final-snapshot"
-    description = "the database snapshot arn"
-    type = string
+    default             = "arn:aws:rds:us-east-1:774443160673:snapshot:fleetcart-final-snapshot"
+    description         = "the database snapshot arn"
+    type                = string
   
 }
 
 variable "database_instance_class" {
-    default = "db.t2.micro"
-    description = "the database instance type"
-    type = string
+    default             = "db.t2.micro"
+    description         = "the database instance type"
+    type                = string
   
 }
 
 variable "database_instance_identifier" {
-    default = "dev-db-1"
-    description = "the database instance/ cluster Name"
-    type = string
+    default             = "dev-db-1"
+    description         = "the database instance/ cluster Name"
+    type                = string
   
 }
 
 variable "multi_az_deployment" {
-    default = false
-    description = "creates a standby db instance"
-    type = bool
+    default             = false
+    description         = "creates a standby db instance"
+    type                = bool
   
 }
 
 # application Load balancer variables
 variable "ssl_certificate_arn" {
-    default = "arn:aws:acm:us-east-1:774443160673:certificate/3a4ae261-bb4e-45d6-a6fa-fafdea2466c1"
-    description = "SSL certificate arn"
-    type = string
+    default             = "arn:aws:acm:us-east-1:774443160673:certificate/3a4ae261-bb4e-45d6-a6fa-fafdea2466c1"
+    description         = "SSL certificate arn"
+    type                = string
   
 }
 
 # SNS topic variables
 variable "operator_email" {
-    default = "ojotosin1900@gmail.com"
-    description = "my email address"
-    type = string
+    default             = "ojotosin1900@gmail.com"
+    description         = "my email address"
+    type                = string
   
 }
 
 # autoscaling group variables
 variable "launch_template_name" {
-    default = "dev-launch-template"
-    description = "launch template name"
-    type = string
+    default             = "dev-launch-template"
+    description         = "launch template name"
+    type                = string
   
 }
 
 variable "ec2_image_id" {
-    default = "ami-0ee3439736243d1e4" // ami id from snapshot created
-    description = "ami id"
-    type = string
+    default             = "ami-0b5ada25b9ba41857" // ami id from snapshot created
+    description         = "ami id"
+    type                = string
   
 }
 
 variable "ec2_instance_type" {
-    default = "t2.micro"
-    description = "ec2 instance type"
-    type = string
+    default             = "t2.micro"
+    description         = "ec2 instance type"
+    type                = string
   
 }
 
 variable "ec2_keypair_name" {
-    default = "npop-Keypair"
-    description = "key pair name"
-    type = string
+    default             = "npop-Keypair"
+    description         = "key pair name"
+    type                = string
   
 }
+
+# route 53 variables
+variable "domain_name" {
+    default             = "expressjapa.com"
+    description         = "domain name of the website"
+    type                = string
+  
+}
+ variable "record_name" {
+    default             = "www"
+    description         = "sub domain name"
+    type                = string
+   
+ }
